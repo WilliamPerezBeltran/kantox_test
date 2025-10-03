dfacbe586a2ec826387734a5ebd919b778fb4300
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2025-10-02 21:45:35 -0500

    Add type validations to Product and corresponding RSpec tests

--- lib/product.rb
+++ lib/product.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true 
 
 Product = Struct.new(:code, :name, :price) do
+  def initialize(code, name, price)
+    raise TypeError, 'Price must be a float' unless price.is_a?(Float)
+    raise TypeError, 'name must be a string' unless name.is_a?(String)
+    raise TypeError, 'code must be a string' unless code.is_a?(String)
+
+    super
+  end
+
   def to_s
     "#{code} - #{name} - £#{format('%.2f', price)}"
   end
