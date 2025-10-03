8381bf66795f8c38eb30b03c96f050b074409546
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2025-10-03 09:03:13 -0500

    Implement basic checkout class

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -2,7 +2,20 @@
 
 # class checkout empty
 class Checkout
-  def initialize(variable_test)
-    @variable_test = variable_test
+  attr_reader :rules, :elements
+  
+  def initialize(rules = [])
+    @rules = rules 
+    @elements = []
+  end
+
+  def scan(product)
+    @elements << product
+  end
+
+  def total
+    return 0.0 if @elements.empty?
+
+    @elements.reduce(0) { |aco, elem| aco + elem.price }.round(2)
   end
 end
